c61974f873f06bba3f9b609cb5694d6d6ce2f00a
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-29 00:33:31 +0200

    Move to bash

--- build-scripts/cmake-dev.sh
+++ build-scripts/cmake-dev.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 srun -w mp-skl2s24c singularity exec -e --nv /mnt/container_store/acc-prac.sif \
     cmake --preset=dev -DBUILD_MCSS_DOCS=OFF
--- build-scripts/run-tests.sh
+++ build-scripts/run-tests.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 srun -w mp-skl2s24c singularity exec -e --nv /mnt/container_store/acc-prac.sif \
     build/dev/test/irregularia_test
