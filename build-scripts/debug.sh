33fb657914d98821dea352a13a8d2293202d47cd
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-29 00:34:31 +0200

    set -e is evil?

--- build-scripts/debug.sh
+++ build-scripts/debug.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-set -e
-
 build-scripts/cmake-dev.sh
 build-scripts/build-dev.sh
 build-scripts/run-tests.sh
--- build-scripts/release.sh
+++ build-scripts/release.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-set -e
-
 build-scripts/cmake-prod.sh
 build-scripts/build-prod.sh
 build-scripts/run-benchmark.sh
