cad91c8dfbafcee2f0fbf3dc02edfc6523fa98fb
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-29 00:29:43 +0200

    Revert to find package impl

--- benchmark/source/irregularia_benchmark.cpp
+++ benchmark/source/irregularia_benchmark.cpp
@@ -1,17 +1,18 @@
 #include <benchmark/benchmark.h>
 
-static void SomeFunction() {
-    int i = 1 == 1;
-}
-
-static void BM_SomeFunction(benchmark::State& state) {
-  // Perform setup here
-  for (auto _ : state) {
-    // This code gets timed
-    SomeFunction();
-  }
+static void BM_StringCreation(benchmark::State& state) {
+  for (auto _ : state)
+    std::string empty_string;
 }
 // Register the function as a benchmark
-BENCHMARK(BM_SomeFunction);
-// Run the benchmark
+BENCHMARK(BM_StringCreation);
+
+// Define another benchmark
+static void BM_StringCopy(benchmark::State& state) {
+  std::string x = "hello";
+  for (auto _ : state)
+    std::string copy(x);
+}
+BENCHMARK(BM_StringCopy);
+
 BENCHMARK_MAIN();
