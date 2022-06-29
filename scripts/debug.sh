febb0f00907b1466d89ba163a4f6dc12e03131fa
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-29 14:59:06 +0200

    Move to new folder

---
+++ scripts/debug.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+scripts/cmake-dev.sh
+scripts/build-dev.sh
+scripts/run-tests.sh
---
+++ scripts/release.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+scripts/cmake-prod.sh
+scripts/build-prod.sh
+scripts/run-benchmarks.sh
