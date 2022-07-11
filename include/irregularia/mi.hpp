39b0082104486fedb976a80f7e45f9e03901b4f7
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-11 22:07:25 +0200

    Implement addition for multiple_int

--- include/irregularia/mi.hpp
+++ include/irregularia/mi.hpp
@@ -11,15 +11,42 @@ struct multiple_int
   using traits = detail::_multiple_int_traits<BitWidth>;
   typename traits::int_type value;
 
-  auto intv() -> typename traits::int_type
+  auto intv() const -> typename traits::int_type
   {
     return this->value & traits::int_mask;
   }
 
-  auto carry() -> typename traits::int_type
+  auto carry() const -> typename traits::int_type
   {
     return this->value & traits::carry_mask;
+  }
+
+  auto flatten_carry() -> void
+  {
+    // TODO: is converting the carry bits into a singular carry bit necessary?
+    if constexpr (irregularia::multiple_int<BitWidth>::traits::carry_width <= 1) {
+      return;
+    }
+
+    if (auto c = this->carry(); c != 0) {
+      // zero out carry bits
+      this->value &= ~traits::carry_mask;
+
+      // toggle one on lowest bit of carry section of value
+      this->value |= (1 << BitWidth);
+    }
   }
 };
 
 };  // namespace irregularia
+
+template<std::size_t BitWidth>
+auto operator+(irregularia::multiple_int<BitWidth> lhs,
+               irregularia::multiple_int<BitWidth> rhs)
+    -> irregularia::multiple_int<BitWidth>
+{
+  auto sum = irregularia::multiple_int<BitWidth>{lhs.value + rhs.value};
+  sum.flatten_carry();
+
+  return sum;
+}
