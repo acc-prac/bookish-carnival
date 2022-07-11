30783d16c4e3b6dfd4dea26969193d12e21b43a5
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-11 15:39:15 +0200

    Add missing typename syntax

--- include/irregularia/milimits.hpp
+++ include/irregularia/milimits.hpp
@@ -9,16 +9,17 @@ template<std::size_t BitWidth>
 struct std::numeric_limits<irregularia::multiple_int<BitWidth>>
 {
 private:
-  using traits = irregularia::multiple_int<BitWidth>::traits;
-  using limits = std::numeric_limits<traits::int_type>;
+  using traits = typename irregularia::multiple_int<BitWidth>::traits;
+  using limits = std::numeric_limits<typename traits::int_type>;
 
   static_assert(limits::is_specialized, "Cannot find numeric limits");
 
 public:
   // TODO: How many more static attrs do we need
   static constexpr bool is_specialized = true;
-  static constexpr bool is_signed = std::is_signed_v<traits::int_type>;
-  static constexpr bool is_integer = std::is_integral_v<traits::int_type>;
+  static constexpr bool is_signed = std::is_signed_v<typename traits::int_type>;
+  static constexpr bool is_integer =
+      std::is_integral_v<typename traits::int_type>;
   static constexpr bool is_exact = true;
 
   static constexpr bool has_infinity = false;
@@ -46,28 +47,31 @@ public:
 
   static constexpr auto max() noexcept -> irregularia::multiple_int<BitWidth>
   {
-    constexpr auto imin = std::numeric_limits<traits::int_type>::max();
+    constexpr auto imin = std::numeric_limits<typename traits::int_type>::max();
     return irregularia::multiple_int<BitWidth> {imin & traits::int_mask};
   }
 
   static constexpr auto epsilon() noexcept
       -> irregularia::multiple_int<BitWidth>
   {
-    constexpr auto eps = std::numeric_limits<traits::int_type>::epsilon();
+    constexpr auto eps =
+        std::numeric_limits<typename traits::int_type>::epsilon();
     return irregularia::multiple_int<BitWidth> {eps};
   }
 
   static constexpr auto epsilon() noexcept
       -> irregularia::multiple_int<BitWidth>
   {
-    constexpr auto re = std::numeric_limits<traits::int_type>::round_error();
+    constexpr auto re =
+        std::numeric_limits<typename traits::int_type>::round_error();
     return irregularia::multiple_int<BitWidth> {re};
   }
 
   static constexpr auto infinity() noexcept
       -> irregularia::multiple_int<BitWidth>
   {
-    constexpr auto inf = std::numeric_limits<traits::int_type>::infinity();
+    constexpr auto inf =
+        std::numeric_limits<typename traits::int_type>::infinity();
     return irregularia::multiple_int<BitWidth> {inf};
   }
 };
