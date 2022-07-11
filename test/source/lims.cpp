0bdff47385dd25e07a7aa2a3edcddedbb65fef5c
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-11 15:45:39 +0200

    Compare against same type

--- test/source/lims.cpp
+++ test/source/lims.cpp
@@ -11,11 +11,11 @@ using limsub7 = std::numeric_limits<irregularia::multiple_int<7>>;
 
 TEST(Limits, Max)
 {
-  EXPECT_EQ((static_cast<std::uint8_t>(1) << 1) - 1, limsub1::max());
-  EXPECT_EQ((static_cast<std::uint8_t>(1) << 2) - 1, limsub2::max());
-  EXPECT_EQ((static_cast<std::uint8_t>(1) << 3) - 1, limsub3::max());
-  EXPECT_EQ((static_cast<std::uint8_t>(1) << 4) - 1, limsub4::max());
-  EXPECT_EQ((static_cast<std::uint8_t>(1) << 5) - 1, limsub5::max());
-  EXPECT_EQ((static_cast<std::uint8_t>(1) << 6) - 1, limsub6::max());
-  EXPECT_EQ((static_cast<std::uint8_t>(1) << 7) - 1, limsub7::max());
+  EXPECT_EQ((static_cast<std::uint8_t>(1) << 1) - 1, limsub1::max().intv());
+  EXPECT_EQ((static_cast<std::uint8_t>(1) << 2) - 1, limsub2::max().intv());
+  EXPECT_EQ((static_cast<std::uint8_t>(1) << 3) - 1, limsub3::max().intv());
+  EXPECT_EQ((static_cast<std::uint8_t>(1) << 4) - 1, limsub4::max().intv());
+  EXPECT_EQ((static_cast<std::uint8_t>(1) << 5) - 1, limsub5::max().intv());
+  EXPECT_EQ((static_cast<std::uint8_t>(1) << 6) - 1, limsub6::max().intv());
+  EXPECT_EQ((static_cast<std::uint8_t>(1) << 7) - 1, limsub7::max().intv());
 }
