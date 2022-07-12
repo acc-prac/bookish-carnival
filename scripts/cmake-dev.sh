a0449dd0c3f4ce1070f76000e416ec8dfc5a9963
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-12 13:29:29 +0200

    Must be executed inside singularity

--- scripts/cmake-dev.sh
+++ scripts/cmake-dev.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-singularity exec -e --nv /mnt/container_store/acc-prac.sif \
-    CXX=$(which nvc++) cmake --preset=dev -DBUILD_MCSS_DOCS=OFF -Wno-dev
+CXX=$(which nvc++) cmake --preset=dev -DBUILD_MCSS_DOCS=OFF -Wno-dev
--- scripts/cmake-prod.sh
+++ scripts/cmake-prod.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-singularity exec -e --nv /mnt/container_store/acc-prac.sif \
-    CXX=$(which nvc++)  cmake -S . -B build/prod -DCMAKE_BUILD_TYPE=Release
+CXX=$(which nvc++) cmake -S . -B build/prod -DCMAKE_BUILD_TYPE=Release
