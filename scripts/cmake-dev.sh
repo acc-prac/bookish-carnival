f118002c550f9632fa0bde033424edeca44b5740
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-12 13:27:40 +0200

    Search for nvc++ compiler

--- scripts/cmake-dev.sh
+++ scripts/cmake-dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 singularity exec -e --nv /mnt/container_store/acc-prac.sif \
-    cmake --preset=dev -DBUILD_MCSS_DOCS=OFF -Wno-dev
+    CXX=$(which nvc++) cmake --preset=dev -DBUILD_MCSS_DOCS=OFF -Wno-dev
--- scripts/cmake-prod.sh
+++ scripts/cmake-prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 singularity exec -e --nv /mnt/container_store/acc-prac.sif \
-    cmake -S . -B build/prod -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_DOWNLOAD_DEPENDENCIES=On
+    CXX=$(which nvc++)  cmake -S . -B build/prod -DCMAKE_BUILD_TYPE=Release
