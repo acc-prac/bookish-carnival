a340e33254df852dc5783e0ec765845d147ab700
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-12 23:46:31 +0200

    Update build script to use carry bit policy

--- scripts/cmake-dev.sh
+++ scripts/cmake-dev.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CXX=$(which nvc++) cmake --preset=dev -DBUILD_MCSS_DOCS=OFF -Wno-dev
+CXX=$(which nvc++) cmake --preset=dev -DBUILD_MCSS_DOCS=OFF -D -DIRREGULARIA_CARRY_BIT_POLICY=1 -Wno-dev
