1005edf8174e39a3b45c5ceb2220375802613a69
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-13 22:24:56 +0200

    Add test for overflowing some of the carry bits

--- test/source/operations.cpp
+++ test/source/operations.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <irregularia/mi.hpp>
 
-TEST(Operations, NonOverflowingAddition)
+TEST(Addition, NonOverflowingSingleIntSingleBit)
 {
   {
     auto l = irregularia::multiple_int<1, 1, std::uint8_t> {0};
@@ -31,7 +31,7 @@ TEST(Operations, NonOverflowingAddition)
   }
 }
 
-TEST(Operations, OverflowingAddition)
+TEST(Addition, OverflowingSingleIntSingleBit)
 {
   auto l = irregularia::multiple_int<1, 1, std::uint8_t> {1};
   auto r = irregularia::multiple_int<1, 1, std::uint8_t> {1};
@@ -48,4 +48,28 @@ TEST(Operations, OverflowingAddition)
     EXPECT_EQ(0, s.intv());
     EXPECT_EQ(0, s.carry());
   }
+}
+
+
+TEST(Addition, PartiallyOverflowing4Ints7Bit)
+{
+  // Max container for 32 bits
+  auto l = irregularia::multiple_int<4, 7, std::uint32_t> {0xEFEFFFFF};
+
+  // Will overflow on the lower two ints, but keep the upper two
+  auto r = irregularia::multiple_int<4, 7, std::uint32_t> {0x01010101};
+
+  auto s = l + r;
+
+  if constexpr (IRREGULARIA_BIT_CARRY_POLICY == 1) {
+    EXPECT_EQ(0xE0E00000, s.intv());
+    EXPECT_EQ(0x00007070, s.carry());
+  }
+
+  else
+  {
+    EXPECT_EQ(0xE0E00000, s.intv());
+    EXPECT_EQ(0x00000000, s.carry());
+  }
+
 }
