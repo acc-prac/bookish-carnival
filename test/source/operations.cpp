a6814ff57d3c0997fafc1ee5c8d158471f16272f
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-13 22:02:16 +0200

    Update tests to check policy

--- test/source/operations.cpp
+++ test/source/operations.cpp
@@ -33,12 +33,19 @@ TEST(Operations, NonOverflowingAddition)
 
 TEST(Operations, OverflowingAddition)
 {
-  {
-    auto l = irregularia::multiple_int<1, 1, std::uint8_t> {1};
-    auto r = irregularia::multiple_int<1, 1, std::uint8_t> {1};
+  auto l = irregularia::multiple_int<1, 1, std::uint8_t> {1};
+  auto r = irregularia::multiple_int<1, 1, std::uint8_t> {1};
 
-    auto s = l + r;
+  auto s = l + r;
+
+  if constexpr (IRREGULARIA_BIT_CARRY_POLICY == 1) {
     EXPECT_EQ(0, s.intv());
     EXPECT_EQ(1 << 1, s.carry());
+  }
+
+  else
+  {
+    EXPECT_EQ(0, s.intv());
+    EXPECT_EQ(0, s.carry());
   }
 }
