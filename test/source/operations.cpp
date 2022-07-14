a3cd2d643a26b601a5b993e084c80cd809c2818f
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-14 15:25:32 +0200

    Fill integer width

--- test/source/operations.cpp
+++ test/source/operations.cpp
@@ -1,11 +1,12 @@
 #include <gtest/gtest.h>
 #include <irregularia/mi.hpp>
+#include <irregularia/milimits.hpp>
 
-TEST(Addition, NonOverflowingFourIntSingleBit)
+TEST(Addition, NonOverflowingOneIntSevenBit)
 {
   {
-    auto l = irregularia::multiple_int<1, std::uint8_t>::encode({0});
-    auto r = irregularia::multiple_int<1, std::uint8_t>::encode({0});
+    auto l = irregularia::multiple_int<7, std::uint8_t>::encode({0});
+    auto r = irregularia::multiple_int<7, std::uint8_t>::encode({0});
 
     auto s = l + r;
     EXPECT_EQ(0, s.intv());
@@ -13,8 +14,8 @@ TEST(Addition, NonOverflowingFourIntSingleBit)
   }
 
   {
-    auto l = irregularia::multiple_int<1, std::uint8_t>::encode({0});
-    auto r = irregularia::multiple_int<1, std::uint8_t>::encode({1});
+    auto l = irregularia::multiple_int<7, std::uint8_t>::encode({0});
+    auto r = irregularia::multiple_int<7, std::uint8_t>::encode({1});
 
     auto s = l + r;
     EXPECT_EQ(1, s.intv());
@@ -22,8 +23,8 @@ TEST(Addition, NonOverflowingFourIntSingleBit)
   }
 
   {
-    auto l = irregularia::multiple_int<1, std::uint8_t>::encode({1});
-    auto r = irregularia::multiple_int<1, std::uint8_t>::encode({0});
+    auto l = irregularia::multiple_int<7, std::uint8_t>::encode({1});
+    auto r = irregularia::multiple_int<7, std::uint8_t>::encode({0});
 
     auto s = l + r;
     EXPECT_EQ(1, s.intv());
@@ -31,10 +32,11 @@ TEST(Addition, NonOverflowingFourIntSingleBit)
   }
 }
 
-TEST(Addition, OverflowingFourIntSingleBit)
+TEST(Addition, OverflowingOneIntSevenBit)
 {
-  auto l = irregularia::multiple_int<1, std::uint8_t>::encode({1});
-  auto r = irregularia::multiple_int<1, std::uint8_t>::encode({1});
+  // Max container for 32 bits, carry bits are off
+  auto l = std::numeric_limits<irregularia::multiple_int<7, std::uint32_t>>::max();
+  auto r = irregularia::multiple_int<7, std::uint8_t>::encode({1});
 
   auto s = l + r;
 
@@ -53,7 +55,7 @@ TEST(Addition, OverflowingFourIntSingleBit)
 TEST(Addition, PartiallyOverflowing4Ints7Bit)
 {
   // Max container for 32 bits, carry bits are off
-  auto l = irregularia::multiple_int<7, std::uint32_t>::encode({0x7F, 0x7F, 0x7F, 0x7F});
+  auto l = std::numeric_limits<irregularia::multiple_int<7, std::uint32_t>>::max();
 
   // Will overflow on the lower two ints, but keep the upper two
   auto r = irregularia::multiple_int<7, std::uint32_t>::encode({0x00, 0x00, 0x01, 0x01});
