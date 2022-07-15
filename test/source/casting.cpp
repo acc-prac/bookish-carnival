72430f902a4dc9cb79aaedceb6c24a118c05f9c9
Timo Wild <timo.wi@aol.com>
2022-07-16 01:50:43 +0200

    Fix wrong expection in test

--- test/source/casting.cpp
+++ test/source/casting.cpp
@@ -8,7 +8,7 @@ TEST(Casting, NoSignAndCarryBits)
 
     irregularia::multiple_int<19, std::uint64_t> t = l;          
 
-    EXPECT_EQ(0b0000'01111111111'110010011'01111111111'100000001'00000000000'001111010, l.intv());
+    EXPECT_EQ(0b0000'00000000000'110010011'00000000000'000000001'00000000000'001111010, l.intv());
     EXPECT_EQ(0, l.carry());
   }
 }
