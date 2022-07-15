1240c95812fa4082749287acdc57805eb3382da1
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-15 19:07:44 +0200

    Missing operator<<

--- test/source/operations.cpp
+++ test/source/operations.cpp
@@ -1,9 +1,8 @@
 #include <bitset>
 
+#include <gtest/gtest.h>
 #include <irregularia/mi.hpp>
 #include <irregularia/milimits.hpp>
-
-#include <gtest/gtest.h>
 
 TEST(Addition, NonOverflowingOneIntSevenBit)
 {
@@ -61,7 +60,8 @@ TEST(Addition, PartiallyOverflowing4Ints7Bit)
   auto l = std::numeric_limits<irregularia::multiple_int<7, std::uint32_t>>::max();
 
   // Will overflow on the lower two ints, but keep the upper two
-  auto r = irregularia::multiple_int<7, std::uint32_t>::encode<int, 4>({0x00, 0x00, 0x01, 0x01});
+  auto r = irregularia::multiple_int<7, std::uint32_t>::encode<int, 4>(
+      {0x00, 0x00, 0x01, 0x01});
 
   auto s = l + r;
 
@@ -77,21 +77,25 @@ TEST(Addition, PartiallyOverflowing4Ints7Bit)
   }
 }
 
-
 TEST(Max, TwoWay)
 {
   using std::max;
 
-  auto l = irregularia::multiple_int<7, std::uint32_t>::encode<int, 4>({0x10, 0x10, 0x01, 0x01});
-  auto r = irregularia::multiple_int<7, std::uint32_t>::encode<int, 4>({0xEF, 0xEF, 0xEF, 0xEF});
+  auto l = irregularia::multiple_int<7, std::uint32_t>::encode<int, 4>(
+      {0x10, 0x10, 0x01, 0x01});
+  auto r = irregularia::multiple_int<7, std::uint32_t>::encode<int, 4>(
+      {0xEF, 0xEF, 0xEF, 0xEF});
 
   auto s = l + r;
 
   // Operators derived from spaceship
 
-  EXPECT_TRUE(l < r) << std::bitset<32>(l.intv()) " < "  << std::bitset<32>(r.intv()) << " failed";
-  EXPECT_TRUE(s < r) << std::bitset<32>(l.intv()) " < "  << std::bitset<32>(r.intv()) << " failed";
-  EXPECT_TRUE(s < l) << std::bitset<32>(l.intv()) " < "  << std::bitset<32>(r.intv()) << " failed";
+  EXPECT_TRUE(l < r) << std::bitset<32>(l.intv()) << " < " << std::bitset<32>(r.intv())
+                     << " failed\n";
+  EXPECT_TRUE(s < r) << std::bitset<32>(l.intv()) << " < " << std::bitset<32>(r.intv())
+                     << " failed\n";
+  EXPECT_TRUE(s < l) << std::bitset<32>(l.intv()) << " < " << std::bitset<32>(r.intv())
+                     << " failed\n";
 
   auto max_lr = max(l, r);
   auto max_sr = max(s, r);
