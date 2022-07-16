a6cd9704e00b3a60caef5fe647fcf3bc0dc45ad5
Timo Wild <timo.wi@aol.com>
2022-07-16 12:16:19 +0200

    Add tests for down_casting with carry-bits

--- test/source/casting.cpp
+++ test/source/casting.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <irregularia/mi.hpp>
 
+//UpCast
 TEST(Casting, UpCast_NoSignAndNoCarryBits)
 {
   {
@@ -97,7 +98,7 @@ TEST(Casting, UpCast_SignAndCarryBits)
   }
 }
 
-
+//DownCast
 TEST(Casting, DownCast_NoCarryBits)
 {
   {
@@ -127,5 +128,43 @@ TEST(Casting, DownCast_NoCarryBits)
 
     EXPECT_EQ(0b01001'00000'00101'01111'00000'00001, t.intv());
     EXPECT_EQ(0, t.carry());
+  }
+}
+
+TEST(Casting, DownCast_WithCarryBits)
+{
+  {
+    auto l = irregularia::multiple_int<7, std::uint16_t>::encode<int, 2>({0b1111101, 0b1000111});
+
+    l = l + l; // Now we have 0b1111010'0001110 and both carry bits are set
+
+    auto t = static_cast<irregularia::multiple_int<3, std::uint8_t>>(l);  
+
+    EXPECT_EQ(0b0010'0110, t.intv());
+    EXPECT_EQ(0b1000'1000, t.carry());
+  }
+
+  {
+    auto l = irregularia::multiple_int<15, std::uint32_t>::encode<int, 2>({0b11101111101101, 0b10010100011000});
+
+    l = l + l; // Now we have 0b11011111011010'00101000110000 and both carry bits are set
+
+    auto t = static_cast<irregularia::multiple_int<7, std::uint16_t>>(l);  
+
+    EXPECT_EQ(0b01011010'00110000, t.intv());
+    EXPECT_EQ(0b10000000'10000000, t.carry());
+  }
+
+  {
+    auto l = irregularia::multiple_int<9, std::uint64_t>::encode<int, 6>({0b110011001, 0b101100000, 
+                                                                          0b101010101, 0b111111111,
+                                                                          0b100000000, 0b100000001});
+
+    auto t = static_cast<irregularia::multiple_int<4, std::uint32_t>>(l);  
+
+    l = l + l; // Now we have 0b100110010'011000000'010101010'111111110'00000000'000000010 and all six carry bits are set
+
+    EXPECT_EQ(0b00010'00000'01010'01110'00000'00010, t.intv());
+    EXPECT_EQ(0b10000'10000'10000'10000'10000'10000, t.carry());
   }
 }
