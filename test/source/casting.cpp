ab9d2e40e325ad739fe0707b792a562cae937f66
Timo Wild <timo.wi@aol.com>
2022-07-16 08:12:09 +0200

    Fix wrong BitWidth

--- test/source/casting.cpp
+++ test/source/casting.cpp
@@ -6,7 +6,7 @@ TEST(Casting, NoSignAndCarryBits)
   {
     auto l = irregularia::multiple_int<3, std::uint8_t>::encode<int, 2>({0b011, 0b000});
 
-    irregularia::multiple_int<6, std::uint16_t> t = l;          
+    irregularia::multiple_int<7, std::uint16_t> t = l;          
 
     EXPECT_EQ(0b00000'011'00000'000, t.intv());
     EXPECT_EQ(0, t.carry());
@@ -15,7 +15,7 @@ TEST(Casting, NoSignAndCarryBits)
   {
     auto l = irregularia::multiple_int<5, std::uint16_t>::encode<int, 2>({0b01111, 0b01010});
 
-    irregularia::multiple_int<10, std::uint32_t> t = l;          
+    irregularia::multiple_int<11, std::uint32_t> t = l;          
 
     EXPECT_EQ(0b00000000'0000000'01111'0000000'01010, t.intv());
     EXPECT_EQ(0, t.carry());
