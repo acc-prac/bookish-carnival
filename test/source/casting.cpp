3b68da8378fb649e98c01fd4ff49bafda0e028c8
Timo Wild <timo.wi@aol.com>
2022-07-16 08:10:47 +0200

    Add more tests

--- test/source/casting.cpp
+++ test/source/casting.cpp
@@ -4,6 +4,24 @@
 TEST(Casting, NoSignAndCarryBits)
 {
   {
+    auto l = irregularia::multiple_int<3, std::uint8_t>::encode<int, 2>({0b011, 0b000});
+
+    irregularia::multiple_int<6, std::uint16_t> t = l;          
+
+    EXPECT_EQ(0b00000'011'00000'000, t.intv());
+    EXPECT_EQ(0, t.carry());
+  }
+
+  {
+    auto l = irregularia::multiple_int<5, std::uint16_t>::encode<int, 2>({0b01111, 0b01010});
+
+    irregularia::multiple_int<10, std::uint32_t> t = l;          
+
+    EXPECT_EQ(0b00000000'0000000'01111'0000000'01010, t.intv());
+    EXPECT_EQ(0, t.carry());
+  }
+
+  {
     auto l = irregularia::multiple_int<9, std::uint32_t>::encode<int, 3>({0b000000001, 0b010010011, 0b001111010});
 
     irregularia::multiple_int<19, std::uint64_t> t = l;          
