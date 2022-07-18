0d7a2ae69612e9d8e8d35295f594319b490878e3
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-18 15:36:57 +0200

    Formatting

--- test/source/downcasting.cpp
+++ test/source/downcasting.cpp
@@ -1,78 +1,56 @@
 
-#include <gtest/gtest.h>
-#include <irregularia/mi.hpp>
-
 #include <bitset>
 #include <iostream>
 
+#include <gtest/gtest.h>
+#include <irregularia/mi.hpp>
 
-//DownCast
-TEST(DownCasting, NoCarryBits)
+// DownCast
+TEST(DownCasting, Simple)
 {
   {
-    auto l = irregularia::multiple_int<7, std::uint16_t>::encode<int, 2>({0b01111101, 0b0000111});
+    auto l = irregularia::multiple_int<7, std::uint16_t>::encode<int, 2>(
+        {0b01111101, 0b0000111});
 
-    auto t = static_cast<irregularia::multiple_int<3, std::uint8_t>>(l);  
+    auto t = static_cast<irregularia::multiple_int<3, std::uint8_t>>(l);
 
     EXPECT_EQ(0b0101'0111, t.intv()) << "Got " << std::bitset<8>(t.intv()) << "\n";
-    EXPECT_EQ(0, t.carry());
+    EXPECT_EQ(0b1000'0000, t.carry());
   }
 
   {
-    auto l = irregularia::multiple_int<15, std::uint32_t>::encode<int, 2>({0b011101111101101, 0b001010100011000});
+    auto l = irregularia::multiple_int<15, std::uint32_t>::encode<int, 2>(
+        {0b011101111101101, 0b001010100011000});
 
-    auto t = static_cast<irregularia::multiple_int<7, std::uint16_t>>(l);  
+    auto t = static_cast<irregularia::multiple_int<7, std::uint16_t>>(l);
 
-    EXPECT_EQ(0b01101101'00011000, t.intv());
-    EXPECT_EQ(0, t.carry());
-  }
-
-  {
-    auto l = irregularia::multiple_int<9, std::uint64_t>::encode<int, 6>({0b110011001, 0b001100000, 
-                                                                          0b101010101, 0b111111111,
-                                                                          0b000000000, 0b100000001});
-
-    auto t = static_cast<irregularia::multiple_int<4, std::uint32_t>>(l);  
-
-    EXPECT_EQ(0b01001'00000'00101'01111'00000'00001, t.intv());
-    EXPECT_EQ(0, t.carry());
+    EXPECT_EQ(0b01101101'00011000, t.intv())
+        << "Got " << std::bitset<16>(t.intv()) << "\n";
+    EXPECT_EQ(0b10000000'00000000, t.carry());
   }
 }
 
-TEST(DownCasting, WithCarryBits)
+/* These tests use IntCount > 2 numbers, meaning the bits will be interleaved */
+TEST(DownCasting, Interleaved)
 {
   {
-    auto l = irregularia::multiple_int<7, std::uint16_t>::encode<int, 2>({0b1111101, 0b1000111});
+    auto l = irregularia::multiple_int<9, std::uint64_t>::encode<int, 6>(
+        {0b110011001, 0b001100000, 0b101010101, 0b111111111, 0b000000000, 0b100000001});
 
-    l = l + l; // Now we have 0b1111010'0001110 and both carry bits are set
+    // 11001
+    // 00000
+    // 10101
+    // 11111
+    // 00000
+    // 00001
 
-    auto t = static_cast<irregularia::multiple_int<3, std::uint8_t>>(l);  
+    // 0b00'11001'11111'00000'00000'10101'00001
 
-    EXPECT_EQ(0b0010'0110, t.intv());
-    EXPECT_EQ(0b1000'1000, t.carry());
-  }
+    auto t = static_cast<irregularia::multiple_int<4, std::uint32_t>>(l);
 
-  {
-    auto l = irregularia::multiple_int<15, std::uint32_t>::encode<int, 2>({0b111101111101101, 0b110010100011000});
-
-    l = l + l; // Now we have 0b111011111011010'100101000110000 and both carry bits are set
-
-    auto t = static_cast<irregularia::multiple_int<7, std::uint16_t>>(l);  
-
-    EXPECT_EQ(0b01011010'00110000, t.intv());
-    EXPECT_EQ(0b10000000'10000000, t.carry());
-  }
-
-  {
-    auto l = irregularia::multiple_int<9, std::uint64_t>::encode<int, 6>({0b110011001, 0b101100000,
-                                                                          0b101010101, 0b111111111,
-                                                                          0b100000000, 0b100000001});
-
-    l = l + l; // Now we have 0b100110010'011000000'010101010'111111110'00000000'000000010 and all six carry bits are set
-
-    auto t = static_cast<irregularia::multiple_int<4, std::uint32_t>>(l);  
-
-    EXPECT_EQ(0b00'00010'00000'01010'01110'00000'00010, t.intv());
-    EXPECT_EQ(0b00'10000'10000'10000'10000'10000'10000, t.carry());
+    EXPECT_EQ(0b00'01001'01111'00000'00000'00101'00001, t.intv())
+        << "Got " << std::bitset<32>(t.intv()) << "\n";
+    EXPECT_EQ(0b00'10000'10000'00000'00000'10000'00000, t.carry())
+        << "Got " << std::bitset<32>(t.carry()) << "\n";
   }
 }
--- test/source/operations.cpp
+++ test/source/operations.cpp
@@ -58,7 +58,8 @@ TEST(Addition, PartiallyOverflowing4Ints7Bit)
   auto l = std::numeric_limits<irregularia::multiple_int<7, std::uint32_t>>::max();
 
   // Will overflow on the lower two ints, but keep the upper two
-  auto r = irregularia::multiple_int<7, std::uint32_t>::encode<int, 4>({0x00, 0x00, 0x01, 0x01});
+  auto r = irregularia::multiple_int<7, std::uint32_t>::encode<int, 4>(
+      {0x00, 0x00, 0x01, 0x01});
 
   auto s = l + r;
 
