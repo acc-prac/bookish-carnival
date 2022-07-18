295c60f46b7b0db3b751dd47ee287a9234e63fa3
Patrick Szegedi <pszegedi@mp-headnode.ziti.uni-heidelberg.de>
2022-07-18 16:38:36 +0200

    Branchless decode

--- test/source/decoding.cpp
+++ test/source/decoding.cpp
@@ -5,19 +5,18 @@ TEST(Decoding, SignedAddition)
 {
 
   {
-    auto l = irregularia::multiple_int<3, std::uint8_t>::encode<int, 2>({2, 1});
-    auto r = irregularia::multiple_int<3, std::uint8_t>::encode<int, 2>({-2, 0});
+    auto l = irregularia::multiple_int<3, std::uint8_t>::encode<2>({2, 1});
+    auto r = irregularia::multiple_int<3, std::uint8_t>::encode<2>({-2, 0});
 
     auto s = l + r;
     
     EXPECT_EQ(0, s.decode()[0]);
     EXPECT_EQ(1, s.decode()[1]);
-    //EXPECT_EQ(0, s.carry()); fails problem with + operator ?
   }
   
   {
-    auto l = irregularia::multiple_int<3, std::uint16_t>::encode<int, 3>({2, 1, -2});
-    auto r = irregularia::multiple_int<3, std::uint16_t>::encode<int, 3>({-2, 1, -2});
+    auto l = irregularia::multiple_int<3, std::uint16_t>::encode<3>({2, 1, -2});
+    auto r = irregularia::multiple_int<3, std::uint16_t>::encode<3>({-2, 1, -2});
 
     auto s = l + r;
     
@@ -27,8 +26,8 @@ TEST(Decoding, SignedAddition)
   }
   
   {
-    auto l = irregularia::multiple_int<7, std::uint32_t>::encode<int, 2>({63, 23});
-    auto r = irregularia::multiple_int<7, std::uint32_t>::encode<int, 2>({-1, 15});
+    auto l = irregularia::multiple_int<7, std::uint32_t>::encode<2>({63, 23});
+    auto r = irregularia::multiple_int<7, std::uint32_t>::encode<2>({-1, 15});
 
     auto s = l + r;
     
