7c45f25a99c39b978dd398fb848b31b66c9b65ca
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-19 17:37:10 +0200

    Add irregularia to benchmarks

--- singularity/small-bench.sh
+++ singularity/small-bench.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
 srun -w mp-skl2s24c singularity exec -e --nv /mnt/container_store/acc-prac.sif \
     build/prod/benchmark/irregularia_benchmark \
-        --benchmark_out_format=csv --benchmark_out=out.csv
+        --benchmark_report_aggregates_only=true \
+        --benchmark_min_time=0.001 --benchmark_out_format=csv \
+        --benchmark_out=out.csv
