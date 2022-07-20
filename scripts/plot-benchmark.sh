4c2650a1594055f51c36e6a31b13db71a0ad54e3
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-20 17:45:19 +0200

    Activate venv

--- scripts/plot-benchmark.sh
+++ scripts/plot-benchmark.sh
@@ -2,6 +2,7 @@
 repdir="benchmark/reports"
 vizdir="benchmark/vizes"
 
+source .venv/bin/activate
 for benchmark in "${repdir}"/*.csv; do
     bname = $(basename "${benchmark}" .csv)
     python3 scripts/plot.py "${benchmark}" "${vizdir}"/"${bname}".png
