c2000ad4154c3359442c18126aa95cef9f28b654
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-20 16:46:56 +0200

    Shorten

--- scripts/big-bench.sh
+++ scripts/big-bench.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-outdir="bench-reports"
-mkdir -p "${outdir}"
+repdir="benchmark/reports"
+vizdir="benchmark/vizes"
+
+mkdir -p "${repdir}" "${vizdir}"
 
 for benchmark in "xpy"
 do
     build/prod/benchmark/irregularia_benchmark \
-        --benchmark_filter="${benchmark}" --benchmark_out="${outdir}"/"${benchmark}".csv \
+        --benchmark_filter="${benchmark}" --benchmark_out="${repdir}"/"${benchmark}".csv \
         --benchmark_min_time=0.001 --benchmark_repetitions=100 \
         --benchmark_report_aggregates_only=true --benchmark_out_format=csv
+
+    python3 scripts/plot.py "${repdir}"/"${benchmark}".csv "${vizdir}"/"${benchmark}".png
 done
