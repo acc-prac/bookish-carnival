07cb8347c3147e767995b9f6489764d348da0783
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-20 17:36:37 +0200

    Run python outside of the accshell

--- scripts/big-bench.sh
+++ scripts/big-bench.sh
@@ -6,12 +6,9 @@ vizdir="benchmark/vizes"
 
 mkdir -p "${repdir}" "${vizdir}"
 
-for benchmark in "xpy-device" "xpy-host"
-do
+for benchmark in "xpy-device" "xpy-host"; do
     build/prod/benchmark/irregularia_benchmark \
         --benchmark_filter="${benchmark}" --benchmark_out="${repdir}"/"${benchmark}".csv \
         --benchmark_min_time=0.001 --benchmark_repetitions=100 \
         --benchmark_report_aggregates_only=true --benchmark_out_format=csv
-
-    python3 scripts/plot.py "${repdir}"/"${benchmark}".csv "${vizdir}"/"${benchmark}".png
 done
