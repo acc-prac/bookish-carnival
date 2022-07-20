20a323c36d21327c9e62a885dd4465fe243e73f6
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-20 16:39:26 +0200

    Add singularity script for executing each benchmark independently

---
+++ scripts/big-bench.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+outdir="bench-reports"
+mkdir -p "${outdir}"
+
+for benchmark in "xpy" "other"
+do
+    build/prod/benchmark/irregularia_benchmark \
+        --benchmark_filter="${benchmark}" --benchmark_out="${outdir}"/"${benchmark}".csv \
+        --benchmark_min_time=0.001 --benchmark_repetitions=100 \
+        --benchmark_report_aggregates_only=true --benchmark_out_format=csv
+done
