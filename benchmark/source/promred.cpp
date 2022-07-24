3afa7195aacc39f94bf5f918202719729f7f7cfa
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-24 23:38:50 +0200

    Add benchmark for testing reductions into a wider datatype

---
+++ benchmark/source/promred.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+
+#include <benchmark/benchmark.h>
+#include <irregularia/accbench_targets.hpp>
+#include <irregularia/mi.hpp>
+#include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
+#include <thrust/host_vector.h>
+
+#include "util.hpp"
+
+static constexpr auto host_par_unseq = thrust::host;
+static constexpr auto device_par_unseq = thrust::cuda::par;
+
+template<auto exec,
+         template<typename value_type>
+         class Container,
+         typename SmallerInteger,
+         typename BiggerInteger,
+         typename ComparingInteger>
+static void sum_prom_int_bench(benchmark::State& state)
+{
+  auto const n_elements = state.range(0);
+  Container<SmallerInteger> const vals(
+      n_elements * ((8 * sizeof(ComparingInteger)) / (BitWidth + 1)), SmallerInteger(1));
+
+  auto const init = BiggerInteger {};
+
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(acc::sum_red(exec, vals.cbegin(), vals.cend(), init));
+  }
+}
