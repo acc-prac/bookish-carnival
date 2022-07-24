d587b19c90908e8a560b5dd913ac8a539c79ffc2
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-24 23:52:00 +0200

    Add irr::mi test

--- benchmark/source/promred.cpp
+++ benchmark/source/promred.cpp
@@ -31,6 +31,28 @@ static void sum_prom_int_bench(benchmark::State& state)
   }
 }
 
+template<auto exec,
+         template<typename value_type>
+         class Container,
+         typename SmallerInteger,
+         typename BiggerInteger>
+static void sum_red_multi_int_bench(benchmark::State& state)
+{
+  auto xs = array_repeat<SmallerInteger::IntCount, int>(1);
+  auto is = array_repeat<BiggerInteger::IntCount, int>(0);
+
+  auto const n_elements = state.range(0);
+  Container<SmallerInteger> const vals(n_elements, T::encode<T::IntCount>(xs));
+  auto const init = BiggerInteger::encode<BiggerInteger::IntCount>(is);
+
+  for (auto _ : state) {
+    auto sum = acc::sum_red(exec, vals.cbegin(), vals.cend(), init);
+    auto subsums = sum.decode();
+
+    benchmark::DoNotOptimize(std::accumulate(subsums.cbegin(), subsums.cend()));
+  }
+}
+
 BENCHMARK(sum_prom_int_bench<host_par_unseq,
                              thrust::host_vector,
                              std::uint32_t,
@@ -39,3 +61,13 @@ BENCHMARK(sum_prom_int_bench<host_par_unseq,
                              32>)
     ->Name("_warmup_")
     ->Arg(1 << 28);
+
+BENCHMARK(sum_red_multi_int_bench<device_par_unseq,
+                                  thrust::device_vector,
+                                  irregularia::multiple_int<7, std::uint32_t>,
+                                  irregularia::multiple_int<15, std::uint64_t>>)
+    ->Name("sumred-device-1-u32x2")
+    ->RangeMultiplier(1 << 2)
+    ->Range(1 << 14, 1 << 28);
+
+BENCHMARK_MAIN();
